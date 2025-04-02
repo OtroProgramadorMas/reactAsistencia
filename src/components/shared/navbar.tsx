598e92bce0bf5df9b4d4867f1c3a5a8ae73e7dee
OtroDevMas <juliancarrascal365@gmail.com>
2025-04-02 13:41:18 -0500

    Login Completo v1

--- src/components/shared/navbar.tsx
+++ src/components/shared/navbar.tsx
@@ -3,8 +3,7 @@ import React from 'react';
 import { 
   AppBar, 
   Toolbar, 
-  Typography, 
-  Button, 
+  Typography,
   IconButton,
   Avatar,
   Menu,
--- src/pages/aprendiz_HomePage.tsx
+++ src/pages/aprendiz_HomePage.tsx
@@ -1,19 +1,24 @@
 // src/pages/AprendizPage.tsx
 import React from 'react';
 import { Container, Typography, Box } from '@mui/material';
+import Navbar from '../components/shared/navbar';
 
 const AprendizHomePage = () => {
+  // datos del localStorage
+  const userData = JSON.parse(localStorage.getItem('userData') || '{}');
   return (
-    <Container maxWidth="md">
-      <Box sx={{ my: 4, textAlign: 'center' }}>
-        <Typography variant="h3" component="h1" gutterBottom>
-          ¡Bienvenido Aprendiz!
-        </Typography>
-        <Typography variant="body1" paragraph>
-          Estamos emocionados de tenerte aquí. Explora todas las funcionalidades disponibles para ti.
-        </Typography>
-      </Box>
-    </Container>
+    <>
+      <Navbar userType="aprendiz" userName={userData.nombre} />
+      <Container maxWidth="md">
+        <Box sx={{ my: 4, textAlign: 'center' }}>
+          <Typography variant="h3" component="h1" gutterBottom>
+            ¡Bienvenido Aprendiz!
+          </Typography>
+          <Typography variant="body1" paragraph>
+            Estamos emocionados de tenerte aquí. Explora todas las funcionalidades disponibles para ti.
+          </Typography>
+        </Box>
+      </Container></>
   );
 };
 
